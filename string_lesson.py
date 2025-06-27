28f3dea1f46f6db46db041795d3b18f8baa3e596
Svetlana Vasilyeva <svetlana.vasilyeva@tietoevry.com>
2025-06-27 16:04:43 +0300

    Working with strings: convert string from  list. Working with f string

---
+++ string_lesson.py
@@ -0,0 +1,20 @@
+mama = 'her name is Sveta'
+print(mama.encode())
+
+mama = 'her name is Sveta'
+len(mama)
+print(len(mama))
+
+string_list_1 = ['her', 'name', 'is', 'Sveta']
+string_1 =  ','.join(string_list_1)
+print(string_1)
+
+print(type(str(60)))
+
+name = 'Kava'
+fstring_exampl = f'Client name is {name}'
+print(fstring_exampl)
+
+age = 55
+fstring_age = f'Client age is {age}'
+print(fstring_age)
