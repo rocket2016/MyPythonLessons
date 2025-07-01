3ab1983ce21feaa19524877c7ec6e391f21716cc
rocket2016 <sema96vasilyev@gmail.com>
2025-07-01 13:58:02 +0300

    working with strings: add todo tasks

--- string_lesson.py
+++ string_lesson.py
@@ -18,3 +18,25 @@ print(fstring_exampl)
 age = 55
 fstring_age = f'Client age is {age}'
 print(fstring_age)
+
+# TODO : Types of strings
+
+# TODO : String comparison
+
+# TODO : Concatenation
+
+# TODO : str methods
+
+# Case Manipulation
+
+# Searching and Replacing
+
+# Splitting and Joining
+
+# Content Checking
+
+# String Cleaning
+
+# Formatting
+
+# TODO : Indexing and slices
