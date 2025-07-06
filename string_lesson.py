1131a743ab5813a2202b9e4c67c125c72ed3ceef
Svetlana Vasilyeva <svetlana.vasilyeva@tietoevry.com>
2025-07-06 14:03:07 +0300

    Working with string: inspecting the difference beween fstring and format method

--- string_lesson.py
+++ string_lesson.py
@@ -70,5 +70,28 @@ name_1 = 'Semen'
 name_2 = 'George'
 format_string_example = "Client name is {1} and {0}".format(name_1, name_2)
 print(format_string_example)
+# 1 and o - indexes
+
+client_group_template = "Client name is {1} and {0}"
+client_group_1 = client_group_template.format('Microsoft', 'Samsung')
+client_group_2 = client_group_template.format('Veres', 'Roshen')
+print(client_group_1 + '\n' + client_group_2)
+#\n - move to new row
+
+client_1 = 'Microsoft'
+client_2 = 'Samsung'
+client_3 = 'Veres'
+client_4 = 'Roshen'
+client_group_1 = f'Client name is {client_1} and {client_2}'
+client_group_2 = f'Client namen is {client_3} and {client_4}'
+
+def create_client_group(client_1, client_2):
+    client_group = f'Client name is {client_1} and {client_2}'
+    return client_group
+
+client_group_1 = create_client_group(client_1 = client_1, client_2 = client_2)
+client_group_2 = create_client_group(client_1 = client_2, client_2 = client_4)
+client_group_3 = create_client_group(client_1 ='Coro', client_2 ='Trifary')
+print(client_group_1 + '\n' + client_group_2 + '\n' + client_group_3)
 
 # TODO : Indexing and slices
